c2eb73250bf226ceca10541612cfc7afacf14101
Michael Krasnov <mihalKrasnov@gmail.com>
2019-05-11 11:40:11 -0400

    Woocommerce
    Added initial support for woocommerce

--- functions.php
+++ functions.php
@@ -94,6 +94,27 @@ if ( ! function_exists( 'min_setup' ) ) :
 			'flex-width'  => true,
 			'flex-height' => true,
 		) );
+
+        function min_add_woocommerce_support() {
+            add_theme_support( 'woocommerce', array(
+                'thumbnail_image_width' => 150,
+                'single_image_width'    => 300,
+
+                'product_grid'          => array(
+                    'default_rows'    => 3,
+                    'min_rows'        => 2,
+                    'max_rows'        => 8,
+                    'default_columns' => 4,
+                    'min_columns'     => 2,
+                    'max_columns'     => 5,
+                ),
+            ) );
+	        add_theme_support( 'wc-product-gallery-zoom' );
+	        add_theme_support( 'wc-product-gallery-lightbox' );
+	        add_theme_support( 'wc-product-gallery-slider' );
+        }
+
+        add_action( 'after_setup_theme', 'min_add_woocommerce_support' );
 	}
 endif;
 add_action( 'after_setup_theme', 'min_setup' );
