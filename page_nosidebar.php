4b149d6569e895228f0c3c9d938e4475f9cda0be
Michael Krasnov <mihalKrasnov@gmail.com>
2019-05-16 13:31:52 -0400

    Fixed fullwidth styling issues

--- page_nosidebar.php
+++ page_nosidebar.php
@@ -10,7 +10,7 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area primary-nosidebar">
 		<main id="main" class="site-main nosidebar">
 
 			<?php
