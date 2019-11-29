62e0e87025122217c0905b0cb4402df2f36f968f
Michael Krasnov <mihalKrasnov@gmail.com>
2019-11-29 11:33:15 -0500

    Refactored CI process, added SCSS support

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,53 @@
-var gulp = require('gulp');
-var zip = require('gulp-zip');
+const gulp = require('gulp');
+const zip = require('gulp-zip');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const cleanCss = require('gulp-clean-css');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const clean = require('gulp-clean');
 
-gulp.task('build', function() {
-    return gulp.src('./src/**')
+sass.compiler = require('node-sass');
+
+gulp.task('copy', function () {
+    return gulp.src(['./src/**/*', '!./src/scss', '!./src/scss/**/*'])
+        .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('clean', function () {
+    return gulp.src('./dist')
+        .pipe(clean({read: false}))
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./src/scss/bundle.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('sass:build', function () {
+    return gulp.src('./src/scss/bundle.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([autoprefixer]))
+        .pipe(cleanCss({compatibility: 'ie8'}))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./src/scss/**/*.scss', ['sass'])
+});
+
+
+gulp.task('dev:watch', function () {
+    return gulp.watch('./src/**/*', gulp.series('clean', 'sass', 'copy'))
+});
+
+gulp.task('dev', gulp.series('clean', 'sass', 'copy', 'dev:watch'));
+
+gulp.task('build', gulp.series('clean', 'sass:build', 'copy'), function() {
+    return gulp.src('./dist/**')
         .pipe(zip('min-dist.zip'))
         .pipe(gulp.dest('./build'));
 });
